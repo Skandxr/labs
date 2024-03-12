4774ce3a6abc14b985737840d0b526db5ef24f25
Skandxr <113800463+Skandxr@users.noreply.github.com>
2024-03-12 14:34:07 +0000

    Add files via upload

---
+++ iteration_challenge.py
@@ -0,0 +1,10 @@
+name_1 = str(input("give me a name"))
+name_2 = str(input("give me another name"))
+name_3 = str(input("and another name"))
+name_4 = str(input("and another"))
+name_5 = str(input("one more"))
+
+name = [name_1, name_2, name_3, name_4, name_5]
+
+for names in name:
+    print(names, "is great")
