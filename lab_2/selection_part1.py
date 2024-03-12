5fb189f27c4447c27fd95ab8440eb34b03bb35da
Skandxr <113800463+Skandxr@users.noreply.github.com>
2024-03-12 14:59:15 +0000

    Add files via upload

---
+++ lab_2/selection_part1.py
@@ -0,0 +1,9 @@
+age = int(input("How old are you?"))
+
+if age >= 18:
+    print("You are in category A")
+if age <18 and age >= 16:
+    print("You are in category B")
+if age <16:
+    print("You are in category C")
+
